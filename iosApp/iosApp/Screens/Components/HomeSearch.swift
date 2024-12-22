8f729ab10c71b7c4224426a85d83fe2ed1714bbf
Rodolfo RUiz <rodolforuyz@gmail.com>
2024-12-21 22:41:36 -0300

    caroussel index

--- iosApp/iosApp/Screens/Components/HomeSearch.swift
+++ iosApp/iosApp/Screens/Components/HomeSearch.swift
@@ -11,7 +11,7 @@ struct HomeSearch: View {
         ScrollView(.horizontal) {
             LazyHStack(spacing: 0) {
                 ForEach(0..<carousselMovie.count, id: \.self) { index in
-                    CarousselImage(carousselImage: carousselMovie.first?.backdropPath ?? "")
+                    CarousselImage(carousselImage: carousselMovie[index].backdropPath)
                 }
             }
         }
@@ -29,6 +29,24 @@ struct CarousselImage: View {
                 Text("Image Load Error")
             } else {
                 ProgressView()
+            }
+        }
+        .frame(width: 256, height: 256)
+        .clipShape(.rect(cornerRadius: 25))
+    }
+}
+
+
+struct TripCardView: View {
+    let destination: String
+    let imageName: String
+    
+    @Binding var isShowDetails: Bool
+    
+    var body: some View {
+        ZStack {
+            GeometryReader { geometry in
+                CarousselImage(carousselImage: imageName)
             }
         }
     }
